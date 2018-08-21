7f8b89db8cb0a78509aff3644d4299a8044a96c0
John Kawahara <kawaharajohn@gmail.com>
2018-08-21 16:59:27 -0700

    Execute PS module 16 Symbols and Structs

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'game_turn'
+require_relative 'treasure_trove'
 
 class Game
 
@@ -21,6 +22,12 @@ class Game
 			puts player
 		end
 		
+		treasures = TreasureTrove::TREASURES
+		puts "\nThere are #{treasures.size} treasures to be found:"
+		treasures.each do |treasure|
+			puts "A #{treasure.name} is worth #{treasure.points} points"
+		end
+
 		1.upto(rounds) do |round|
 			puts"\nRound #{round}:"
 			@players.each do |player|
