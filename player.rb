2656ed20417861a197d485abcd713178b31ee9a8
John Kawahara <kawaharajohn@gmail.com>
2018-08-23 14:21:26 -0700

    Execute PS module 17 Hashes

--- player.rb
+++ player.rb
@@ -5,10 +5,11 @@
 	def initialize(name, health=100)
 		@name = name.capitalize
 		@health = health
+		@found_treasures = Hash.new(0)
 	end
 
 	def to_s
-		"I'm #{@name} with a health of #{@health} and a score of #{score}"
+		"I'm #{@name} with health = #{@health}, points = #{points}, and score = #{score}"
 	end
 
 	def blam
@@ -22,7 +23,7 @@
 	end
 
 	def score
-		@health + @name.length
+		@health + points
 	end
 
 	def strong?
@@ -31,6 +32,16 @@
 
 	def <=>(other)
 		other.score <=> score
+	end
+
+	def found_treasure(treasure)
+		@found_treasures[treasure.name] += treasure.points
+		puts "#{@name} found a #{treasure.name} worth #{treasure.points} points."
+		puts "#{@name}'s treasures: #{@found_treasures}"
+	end
+
+	def points
+		@found_treasures.values.reduce(0, :+)
 	end
 end
 
--- player_spec.rb
+++ player_spec.rb
@@ -1,5 +1,6 @@
 require_relative 'spec_helper'
 require_relative 'player'
+require_relative 'treasure_trove'
 
 describe Player do
 
@@ -16,13 +17,19 @@ describe Player do
 	it "has an initial health" do
 		expect(@player.health).to eq(150)
 	end
-
+	
 	it "has a string representation" do
-		expect(@player.to_s).to eq("I'm Larry with a health of 150 and a score of 155")
+		@player.found_treasure(Treasure.new(:hammer, 50))
+		@player.found_treasure(Treasure.new(:hammer, 50))
+	
+		expect(@player.to_s).to eq("I'm Larry with health = 150, points = 100, and score = 250")
 	end
 
-	it "computes a score as the sum of its health and length of name" do
-		expect(@player.score).to eq(150 + 5)
+	it "computes a score as the sum of its health and points" do
+		@player.found_treasure(Treasure.new(:hammer, 50))
+		@player.found_treasure(Treasure.new(:hammer, 50))
+	
+		@player.score.should == 250
 	end
 
 	it "increases health by 15 when w00ted" do
@@ -60,14 +67,36 @@ describe Player do
 	context "in a collection of players" do
 		before do
 			@player1 = Player.new("moe", 100)
-		    @player2 = Player.new("larry", 200)
-		    @player3 = Player.new("curly", 300)
-
-		    @players = [@player1, @player2, @player3]
+			@player2 = Player.new("larry", 200)
+			@player3 = Player.new("curly", 300)
+			@players = [@player1, @player2, @player3]
 		end
 
 		it "is sorted by decreasing score" do
 		    expect(@players.sort).to eq(@players.sort)
 		end
+	end
+
+	it "computes points as the sum of all treasure points" do
+		expect(@player.points).to eq(0)
+	
+		@player.found_treasure(Treasure.new(:hammer, 50))
+	
+		expect(@player.points).to eq(50)
+	
+		@player.found_treasure(Treasure.new(:crowbar, 400))
+	
+		expect(@player.points).to eq(450)
+	
+		@player.found_treasure(Treasure.new(:hammer, 50))
+	
+		expect(@player.points).to eq(500)
+	end
+
+	it "computes a score as the sum of its health and points" do
+		@player.found_treasure(Treasure.new(:hammer, 50))
+		@player.found_treasure(Treasure.new(:hammer, 50))
+	
+		expect(@player.score).to eq(250)
 	end
 end
